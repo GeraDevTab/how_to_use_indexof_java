0b3a805ed1f6956a6e8bf60ed23bf8060015e3a4
Gerardo <everythingissofast@gmail.com>
2025-08-05 08:15:37 -0600

    second commit add others examples of indexOf

--- src/IndexOfExample.java
+++ src/IndexOfExample.java
@@ -7,5 +7,13 @@ public class IndexOfExample {
         int index2 = s1.indexOf("index");  //retunrs the index of index substring
 
         System.out.println(index1 + " " + index2); //2 8
+
+        //passing substring with from index
+        int index3 = s1.indexOf("is",4); // returns the index of is substring after 4th index
+        System.out.println(index3); // 5 i.e. the index of another is
+
+        //passing char value
+        int index4 = s1.indexOf('s'); //returns the index of s char value
+        System.out.println(index4); //3
     }
 }
